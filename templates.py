eb81a2c0b8044ee91cd5dedae925a775491e2733
yun-sangho <contact@yunsangho.com>
2019-03-01 12:37:07 +0900

    delete the temaplate does not exist

--- templates.py
+++ templates.py
@@ -5,11 +5,5 @@ templates = [
             "week1": ["day1", "day2", "day3"],
             "week2": ["day1", "day2", "day3", "day4"],
         },
-    },
-    {
-        "name": "school",
-        "schedule": {
-            "week1": ["day1", "day2", "day3", "day4", "day5"]
-        },
-    },
+    }
 ]
